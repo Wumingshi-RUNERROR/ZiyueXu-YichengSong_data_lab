6735c595dc7895346780d00a96fc9659164eb628
Ziyue_Xu <zx1790@nyu.edu>
2026-01-29 15:05:47 -0500

    3

--- part2_dicts_as_rows.py
+++ part2_dicts_as_rows.py
@@ -11,20 +11,29 @@ students = [
 ]
 
 # TODO 1: Print all student names
-
+for student in students:
+    print(student["name"])
 # TODO 2: Print only GSAS students (name + credits)
-
+for student in students:
+    if student["school"]=="GSAS":
+        print(student["name"],student["credits"])
 # TODO 3: Add a new field to each row:
 #   status='full-time' if credits >= 30 else 'part-time'
-
+for i,student in enumerate(students):
+    students[i]["status"]="full-time" if student["credits"]>=30 else "part-time"
+    
 # TODO 4: Update Bob’s credits to 30 AND ensure his status updates correctly
-
+for i,student in enumerate(students):
+    if student["name"]=="Bob":
+        students[i]["credits"]=30
+        students[i]["status"]="full-time"
 def get_students_by_school(rows, school):
     """Return a list of rows where row['school'] == school."""
     # TODO 5: implement
-    return []
+    return [row for row in rows if row["school"==school]]
 
 # TODO 6: Call get_students_by_school(students, "GSAS") and print results
-
-# Reflection (answer in a comment):
+get_students_by_school(students,"GSAS")
+# Reflection (answer in a comment): what
 # TODO: Why is dict-based code safer than index-based list code?
+
