bdd4ba7b8d0b175881c41f0bf1a3a07dc7dbbbba
Ziyue_Xu <zx1790@nyu.edu>
2026-01-29 15:05:14 -0500

    1

--- challenge_extension.py
+++ challenge_extension.py
@@ -15,14 +15,35 @@ students = [
 
 # Challenge 1: Build a school index: school -> list of student rows
 # TODO
-
+school_to_students={}
+for student in students:
+    if student["school"] in school_to_students:
+        school_to_students[student["school"]].append(student)
+    else:
+        school_to_students[student["school"]]=[student]
 # Challenge 2: Enforce unique ids on insert
 # Write insert_student(students, index_by_id, new_row) that rejects duplicate id
 # TODO
+def insert_student(students, index_by_id, new_row):
+    return
 
 # Challenge 3: Mini query function:
 # query(rows, school=None, min_credits=None) -> list of matching rows
 # TODO
+def query(rows, school=None, min_credits=None):
+    if not school:
+        return [student for student in rows if student["credits"]>=min_credits]
+    elif not min_credits:
+        return [student for student in rows if student["school"]==school]
+    else:
+        return [student for student in rows if student["school"]==school and student["credits"]>=min_credits]
+
 
 # Challenge 4 (optional): Persist to disk as TSV using the csv module
 # TODO
+import csv
+with open('names.tsv', 'w', encoding='utf-8', newline='') as f:
+    fieldnames = ['id', 'name','school','credits']
+    writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter='\t')
+    writer.writeheader()
+    writer.writerows(students)
